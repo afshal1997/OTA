7d18e1c267aa031074be5d9d597d4b12244e17e1
afshal1997 <afshal.warsi@shottechnologies.co>
2021-10-14 01:32:48 +0500

    pricing page

--- src/Components/Common/Tabs/VerticalTabs.js
+++ src/Components/Common/Tabs/VerticalTabs.js
@@ -38,8 +38,64 @@ const VerticalTabs = () => {
                 </TabList>
 
                 <TabPanel>
-                    <div>
-                        <h2>Any content 1</h2>
+                    <div className="pt-3 pb-3">
+                        <div class="mt-5 row">
+                            <div class="text-center col-lg-4">
+                                <div class="card PricingCard BrandFont pt-0 ">
+                                    <div class="PackageAndPricing">
+                                        <h4>Logo Design (simple)</h4>
+                                        <h3><span>USD -</span>$50 </h3>
+                                    </div>
+                                    <ul>
+                                        <li>2 Original Logo Concepts</li>
+                                        <li>4 Revisions</li>
+                                        <li>100% Ownership Rights</li>
+                                        <li> JPEG and PSD</li>
+                                        <li>48-72 Hours Turn Around Time</li>
+                                    </ul>
+
+                                    <a class="PricingApplyNowBtn w-100 p-2" href="javascript:void(0)">Outsource Now</a>
+                                </div>
+                            </div>
+                            <div class="text-center col-lg-4">
+                                <div class="card PricingCard BrandFont pt-0 ActivePricingCard">
+                                    <div class="PackageAndPricing">
+                                        <h4>Mascot Logo</h4>
+                                        <h3><span>USD -</span>$150 </h3>
+                                    </div>
+                                    <ul>
+                                        <li>(1) Mascot Logo</li>
+                                        <li>10 Revisions</li>
+                                        <li>Hand drawn Sketch</li>
+                                        <li>Presentation on mockup</li>
+                                        <li>2 days' time delivery</li>
+                                        <li>Revision time Delivery (24 hours)</li>
+                                        <li>Mobile Responsive</li>
+                                        <li>Free Logo Design</li>
+                                        <li>After Sales Support</li>
+                                        <li> File Formats: Ai,Eps,Png,Psd,Jpeg,Pdf</li>
+                                    </ul>
+
+                                    <a class="PricingApplyNowBtn w-100 p-2" href="javascript:void(0)">Outsource Now</a>
+                                </div>
+                            </div>
+                            <div class="text-center col-lg-4">
+                                <div class="card PricingCard BrandFont pt-0 ">
+                                    <div class="PackageAndPricing">
+                                        <h4>Flyer (Single Leaflet)</h4>
+                                        <h3><span>USD -</span>$50 </h3>
+                                    </div>
+                                    <ul>
+                                        <li>1 Design Concept</li>
+                                        <li>Turn Around 48-72 Hours</li>
+                                        <li>1 Stock Photo</li>
+                                        <li>Free Unlimited Revisions</li>
+                                    </ul>
+
+                                    <a class="PricingApplyNowBtn w-100 p-2" href="javascript:void(0)">Outsource Now</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </TabPanel>
                 <TabPanel>
@@ -63,7 +119,7 @@ const VerticalTabs = () => {
                                         <div className="cardText pt-5 pb-5">
                                             <h4>USD $8-12<span>/hour</span></h4>
                                             <h5>Android Development</h5>
-                                            <a class="PricingApplyNowBtn w-100 p-2" href="abc">Outsource Now</a>
+                                            <a class="PricingApplyNowBtn w-100 p-2" href="javascript:void(0)">Outsource Now</a>
                                         </div>
                                     </Card.Body>
                                 </Card>
@@ -81,7 +137,7 @@ const VerticalTabs = () => {
                                         <div className="cardText pt-5 pb-5">
                                             <h4>USD $8-12<span>/hour</span></h4>
                                             <h5>Android Development</h5>
-                                            <a class="PricingApplyNowBtn w-100 p-2" href="abc">Outsource Now</a>
+                                            <a class="PricingApplyNowBtn w-100 p-2" href="javascript:void(0)">Outsource Now</a>
                                         </div>
                                     </Card.Body>
                                 </Card>
@@ -99,7 +155,7 @@ const VerticalTabs = () => {
                                         <div className="cardText pt-5 pb-5">
                                             <h4>USD $8-12<span>/hour</span></h4>
                                             <h5>Android Development</h5>
-                                            <a class="PricingApplyNowBtn w-100 p-2" href="abc">Outsource Now</a>
+                                            <a class="PricingApplyNowBtn w-100 p-2" href="javascript:void(0)">Outsource Now</a>
                                         </div>
                                     </Card.Body>
                                 </Card>
