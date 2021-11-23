e285b4a58eb251de4a017bedd410f6c60f832ab5
Abdul Basit <abdul.basit@shot.local>
2021-11-23 12:45:03 -0800

    web development images added

--- src/Layout/PortfolioPageLayout/DevelopYourBrand.js
+++ src/Layout/PortfolioPageLayout/DevelopYourBrand.js
@@ -8,6 +8,14 @@ import Shape1 from "../../Assets/Portfolio/shape_6.png";
 import Shape2 from "../../Assets/Portfolio/shape_8.png";
 import Shape3 from "../../Assets/Portfolio/shape_9.png";
 import Akaria from "../../Assets/Portfolio/WebDevelopment/Akaria.jpg";
+import BlackBoxMedia from "../../Assets/Portfolio/WebDevelopment/BlackBoxMedia.jpg";
+import BusinessMadeEazi from "../../Assets/Portfolio/WebDevelopment/BusinessMadeEazi.jpg";
+import CalsAutoRepairCenter from "../../Assets/Portfolio/WebDevelopment/CalsAutoRepairCenter.jpg";
+import DopeStudios from "../../Assets/Portfolio/WebDevelopment/DopeStudios.jpg";
+import FamilyGovernmentAgency from "../../Assets/Portfolio/WebDevelopment/FamilyGovernmentAgency.jpg";
+import knitTech from "../../Assets/Portfolio/WebDevelopment/knitTech-Inc.jpg";
+import MarvelMarketing from "../../Assets/Portfolio/WebDevelopment/MarvelMarketing.jpg";
+import RolixAutomotive from "../../Assets/Portfolio/WebDevelopment/RolixAutomotive.jpg";
 const DevelopYourBrand = () => {
   useEffect(() => {
     AOS.init({
@@ -122,46 +130,51 @@ const DevelopYourBrand = () => {
                 </Col>
                 <Col lg={4}>
                   <div className="portfolioTabsCards">
-                    <img className="w-100" src={Akaria} alt=""></img>
+                    <img className="w-100" src={BlackBoxMedia} alt=""></img>
                   </div>
                 </Col>
                 <Col lg={4}>
                   <div className="portfolioTabsCards">
-                    <img className="w-100" src={Akaria} alt=""></img>
-                  </div>
-                </Col>
-              </Row>
-              <Row>
-                <Col lg={4}>
-                  <div className="portfolioTabsCards">
-                    <img className="w-100" src={Akaria} alt=""></img>
-                  </div>
-                </Col>
-                <Col lg={4}>
-                  <div className="portfolioTabsCards">
-                    <img className="w-100" src={Akaria} alt=""></img>
-                  </div>
-                </Col>
-                <Col lg={4}>
-                  <div className="portfolioTabsCards">
-                    <img className="w-100" src={Akaria} alt=""></img>
+                    <img className="w-100" src={BusinessMadeEazi} alt=""></img>
                   </div>
                 </Col>
               </Row>
               <Row>
                 <Col lg={4}>
                   <div className="portfolioTabsCards">
-                    <img className="w-100" src={Akaria} alt=""></img>
+                    <img
+                      className="w-100"
+                      src={CalsAutoRepairCenter}
+                      alt=""
+                    ></img>
                   </div>
                 </Col>
                 <Col lg={4}>
                   <div className="portfolioTabsCards">
-                    <img className="w-100" src={Akaria} alt=""></img>
+                    <img className="w-100" src={DopeStudios} alt=""></img>
                   </div>
                 </Col>
                 <Col lg={4}>
                   <div className="portfolioTabsCards">
-                    <img className="w-100" src={Akaria} alt=""></img>
+                    <img className="w-100" src={FamilyGovernmentAgency} alt=""></img>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col lg={4}>
+                  <div className="portfolioTabsCards">
+                    <img className="w-100" src={knitTech} alt=""></img>
+                  </div>
+                </Col>
+
+                <Col lg={4}>
+                  <div className="portfolioTabsCards">
+                    <img className="w-100" src={MarvelMarketing} alt=""></img>
+                  </div>
+                </Col>
+                <Col lg={4}>
+                  <div className="portfolioTabsCards">
+                    <img className="w-100" src={RolixAutomotive} alt=""></img>
                   </div>
                 </Col>
               </Row>
