4bb972d5e75dd6ba5bbc778d1a4fa38bdf20ea30
Afshal Warsi <afshal.warsi@shot.local>
2021-11-24 05:29:27 +0500

    portfolio page Tabs Done

--- src/Components/Pages/Portfolio.js
+++ src/Components/Pages/Portfolio.js
@@ -2,13 +2,14 @@ import React from 'react'
 import DevelopYourBrand from '../../Layout/PortfolioPageLayout/DevelopYourBrand'
 import PortfolioBrand from '../../Layout/PortfolioPageLayout/PortfolioBrand'
 import PortfolioMainBanner from '../../Layout/PortfolioPageLayout/PortfolioMainBanner'
-
+import CallCenterChooseUs from '../../Layout/PlansPageLayout/CallCenter/CallCenterChooseUs'
 const Portfolio = () => {
     return (
         <div>
             <PortfolioMainBanner />
             <PortfolioBrand />
-            <DevelopYourBrand/>
+            <DevelopYourBrand />
+            <CallCenterChooseUs />
         </div>
     )
 }
