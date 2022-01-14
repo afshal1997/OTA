231480e407d99a0bd856d53aa78946cbad0646e4
Abdul Basit <abdul.basit@shottech.co>
2022-01-15 02:27:17 +0500

    loader updates

--- src/Components/Common/Loader/Loader.js
+++ src/Components/Common/Loader/Loader.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loader = () => {
    return (
-      <div className="d-flex justify-content-center align-items-center h100 bg-secondary">
+      <div className="d-flex justify-content-center align-items-center h100 bg-dark pre-loader">
          <div class="spinner-border text-primary" role="status">
             <span class="sr-only">Loading...</span>
          </div>
