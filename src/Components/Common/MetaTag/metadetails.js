e16f57ee6878c1ffdad73f3f617e235430e1e631
abdulbasit001 <abdul.basit@shottechnologies.co>
2022-01-26 01:05:35 +0500

    updatesa

---
+++ src/Components/Common/MetaTag/metadetails.js
@@ -0,0 +1,86 @@
+export const metaDetails = {
+   'home': {
+      title: '',
+      description: ''
+   },
+   'about': {
+      title: '',
+      description: ''
+   },
+   'white-labelling': {
+      title: '',
+      description: ''
+   },
+   'digital-startup': {
+      title: '',
+      description: ''
+   },
+   'resource-outsourcing': {
+      title: '',
+      description: ''
+   },
+   'call-center': {
+      title: '',
+      description: ''
+   },
+   'creative-design': {
+      title: '',
+      description: ''
+   },
+   'web-development': {
+      title: '',
+      description: ''
+   },
+   'digital-marketing': {
+      title: '',
+      description: ''
+   },
+   'app-development': {
+      title: '',
+      description: ''
+   },
+   'video-development': {
+      title: '',
+      description: ''
+   },
+   'content-writing': {
+      title: '',
+      description: ''
+   },
+   'cyber-security': {
+      title: '',
+      description: ''
+   },
+   'case-studies': {
+      title: '',
+      description: ''
+   },
+   'pricing': {
+      title: '',
+      description: ''
+   },
+   'portfolio': {
+      title: '',
+      description: ''
+   },
+   'blog': {
+      title: '',
+      description: ''
+   },
+   'contact-us': {
+      title: '',
+      description: ''
+   },
+   'terms-and-conditions': {
+      title: '',
+      description: ''
+   },
+   'privacy-policy': {
+      title: '',
+      description: ''
+   },
+   'refund-policy': {
+      title: '',
+      description: ''
+   }
+}
