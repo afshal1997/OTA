7ac04d6c5bd3ab47ee84fa116dffad1e0e938896
abdulbasit001 <abdul.basit@shottechnologies.co>
2022-02-16 20:23:53 +0500

    updates

--- src/Constants/popularPostsSlides.js
+++ src/Constants/popularPostsSlides.js
@@ -3,6 +3,7 @@ import BlogPageImge02 from "./../Assets/Blog/BlogPageImge02.png";
 import BlogPageImge03 from "./../Assets/Blog/BlogPageImge03.png";
 import BlogPageImge04 from "./../Assets/Blog/BlogPageImge04.png";
 import BlogPageImge05 from "./../Assets/Blog/BlogPageImge05.png";
+import BlogPageImge06 from "./../Assets/Blog/dm.png";
 export const popularPostsSlides = [
   {
     slideImage: BlogPageImge01,
@@ -19,14 +20,14 @@ export const popularPostsSlides = [
   {
     slideImage: BlogPageImge03,
     slideTitle: 'SEO',
-    date: "February 09, 2021",
+    date: "February 09, 2022",
     details: 'The demand for online stores boomed in 2021, regardless of the size and niche of the business. Going into 2022, this trend is likely to continue ...'
 
   },
   {
     slideImage: BlogPageImge04,
     slideTitle: 'Website Development',
-    date: "February 09, 2021",
+    date: "February 09, 2022",
     details: "While living in the digital world, any business owner needs a business website or custom web development services unless they are not ..."
 
 
@@ -34,10 +35,14 @@ export const popularPostsSlides = [
   {
     slideImage: BlogPageImge05,
     slideTitle: 'Website Designing',
-    date: "February 09, 2021",
+    date: "February 09, 2022",
     details: "48% of individuals accept web design as the main element in choosing a business' credibility. Your website is the foundation of your brand’s ..."
-
-
+  },
+  {
+    slideImage: BlogPageImge06,
+    slideTitle: 'Digital Marketing',
+    date: "February 16, 2022",
+    details: "In this pandemic-hit world, when more and more businesses are shifting online, it's all about reaching out to the targeted audience ..."
   }
 
 ];
